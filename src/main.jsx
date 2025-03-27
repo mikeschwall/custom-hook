22322ff5cc18b6fa85eb0aa2335c229af6b48d79
Mike Schwall <mikschwall@gmail.com>
2025-03-27 08:30:23 -0500

    starter project

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './bootstrap.css'
+import App from './App.jsx'
+import { Provider } from 'react-redux'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App/>
+  </StrictMode>
+  
+    ,
+)
